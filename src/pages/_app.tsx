9867a7bb583be736743849df908c9c41c9fa7d4f
ducan3007 <lordmasterking001@gmail.com>
2022-10-27 16:15:44 +0700

    hustky

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,7 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import MUIButton from 'components/MUIButton';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const a = () => {};
-
   return <Component {...pageProps} />;
 }
 
